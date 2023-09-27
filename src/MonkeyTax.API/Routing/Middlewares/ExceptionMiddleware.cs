2d7318d7ba33311ad92f91231a74517532069d77
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2023-09-26 21:59:35 -0300

    Change serialization method in ExceptionMiddleware

--- src/MonkeyTax.API/Routing/Middlewares/ExceptionMiddleware.cs
+++ src/MonkeyTax.API/Routing/Middlewares/ExceptionMiddleware.cs
@@ -1,7 +1,7 @@
 ﻿using MonkeyTax.API.Routing.Model;
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 using System.Net;
-using System.Text.Json;
 
 namespace MonkeyTax.API.Routing.Middlewares
 {
@@ -37,7 +37,7 @@ namespace MonkeyTax.API.Routing.Middlewares
                     Exception = ex,
                 };
 
-                var result = JsonSerializer.Serialize(errorResponse);
+                var result = JsonConvert.SerializeObject(errorResponse);
                 context.Response.StatusCode = errorResponse.StatusCode;
                 context.Response.ContentType = "application/json";
                 await context.Response.WriteAsync(result);
